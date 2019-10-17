70dc7cc110d563132e80a94e914f0c0aa01b6e91
王琳飞 <linfei6@staff.sina.com.cn>
2019-10-17 16:56:59 +0800

    __DEPLOY__

--- server.js
+++ server.js
@@ -44,8 +44,8 @@ app.use('/*', function (req, res, next) {
         res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
         res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
         res.header('Access-Control-Allow-Credentials', true);
-        util.reqcookie && util.reqcookie(req, res)
     }
+    util.reqcookie && util.reqcookie(req, res)
     next();
 });
 app.use('', musicApi);
